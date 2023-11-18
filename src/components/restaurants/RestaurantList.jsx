9c69332f051fb360dc416f981fe44210f74354ea
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-18 11:55:34 +0530

    Home Page & Restaurant List added

---
+++ src/components/restaurants/RestaurantList.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const RestaurantList = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://localhost:8900/crud/get-restaurants").then((result) => {
+      setRestaurants(result.data.data);
+    });
+  }, [isLoading]);
+
+  const restaurantsUICards = restaurants?.map((restaurant) => (
+    <div
+      key={restaurant.id}
+      className="bg-gray-200 p-6 rounded-md shadow-md w-72"
+    >
+      <h2 className="text-xl font-semibold mb-2">{restaurant.name}</h2>
+      <p className="text-gray-600 mb-2">{restaurant.address}</p>
+      <p className="text-gray-900">{`Contect-us: ${restaurant.contact}`}</p>
+      <div className="flex justify-between mt-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-400">
+          Edit
+        </button>
+        <button
+          className="bg-red-700 text-white px-4 py-2 rounded-md font-semibold hover:bg-red-400"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
+
+ 
+
+  return (
+    <div className="my-8">
+      <h1 className="text-3xl font-bold text-center mb-4">
+        Choose Your Favorite Restaurant
+      </h1>
+      <div className="flex flex-wrap justify-center gap-6">
+        {restaurantsUICards}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,28 @@
+import RestaurantList from "../components/restaurants/restaurantList";
 
 const HomePage = () => {
   return (
-    <>
-    <div>hi</div>
-    </>
-  )
-}
+    <main>
+      <section className="bg-gray-500 text-white h-[92vh]  flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold mb-4">
+            Welcome to Resto-Rover
+          </h1>
+          <p className="text-lg mb-8">
+            We believe good food brings great smiles! 😊
+          </p>
+          <button
+            className="bg-rose-600 text-white px-6 py-3 rounded-full font-semibold hover:bg-rose-500"
+          >
+            <a href="#restaurants">Explore Restaurants</a>
+          </button>
+        </div>
+      </section>
+      <section id="restaurants">
+        <RestaurantList />
+      </section>
+    </main>
+  );
+};
 
 export default HomePage;
